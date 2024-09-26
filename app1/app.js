30b344f949271becdea998b8ae8cf69aabcddefe
Valhe Kouneli <valhe.kouneli@gmail.com>
2024-09-26 20:38:11 +0300

    Nodeapp endpoint returns required info

--- app1/app.js
+++ app1/app.js
@@ -1,9 +1,29 @@
 import express from "express";
+import os from "os";
+import { $ } from 'zx';
 
 const app = express();
 
-app.get('/', function (req, res) {
-    res.send('TODO');
+app.get('/', async function (req, res) {
+    let info = "";
+    info += "<p>uptime: " + os.uptime() + "</p>\n";
+
+    info += "<p>network " + os.networkInterfaces().lo[0].address + "</p>\n";
+    const psAuxOutput = await $`ps aux`;
+
+    function encode(e){return e.replace(/[^]/g,function(e){return"&#"+e.charCodeAt(0)+";"})}
+
+    const infoRows = psAuxOutput.toString().split("\n");
+    for (const nbr in infoRows) {
+        info += "<p>" + encode(infoRows[nbr]) + "</p>\n";
+    }
+    const dfhOutput = await $`df -h`;
+    const infoRows2 = dfhOutput.toString().split("\n");
+    for (const nbr in infoRows2) {
+        info += "<p>" + encode(infoRows2[nbr]) + "</p>\n";
+    }
+    res.send(info);
+    
 });
 
 app.listen(3000, function () {
