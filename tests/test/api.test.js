41246a2f652e4c1d4096f52d77e1ef10ba640ac8
Valhe Kouneli <valhe.kouneli@gmail.com>
2025-01-25 21:02:19 +0200

    Create a test for logging in

--- tests/test/api.test.js
+++ tests/test/api.test.js
@@ -1,19 +1,30 @@
 import { expect } from "chai";
 import axios from 'axios';
 
-const BASE_URL = `http://${process.env.RUNNING_LOCATION || 'localhost'}:8198`;
+const BASE_URL = `${process.env.RUNNING_LOCATION || 'localhost'}:8198`;
 
 describe('nginx', () => {
     it('should return 401 without credentials', async () => {
         let response;
         try {
-            const res = await axios.get(`${BASE_URL}`);
-            res = res;
+            response = await axios.get(`http://${BASE_URL}`);
         } catch (e) {
             expect(e).to.have.property('response');
             response = e.response;
         }
         expect(response).to.have.property('status');
         expect(response.status).to.eql(401);
+    });
+
+    it('should return 200 with credentials', async () => {
+        let response;
+        try {
+            response = await axios.get(`http://test:test@${BASE_URL}`);
+        } catch (e) {
+            expect(e).to.have.property('response');
+            response = e.response;
+        }
+        expect(response).to.have.property('status');
+        expect(response.status).to.eql(200);
     });
 });
