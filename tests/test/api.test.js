332c6b4a8dee83eabc82b35da1bb9d4b358b1084
Valhe Kouneli <valhe.kouneli@gmail.com>
2025-01-25 17:41:28 +0200

    Test for request response. Send requests to address defined by env var.

--- tests/test/api.test.js
+++ tests/test/api.test.js
@@ -1,11 +1,19 @@
 import { expect } from "chai";
 import axios from 'axios';
 
-const BASE_URL = 'http://localhost:8198';
+const BASE_URL = `http://${process.env.RUNNING_LOCATION || 'localhost'}:8198`;
 
 describe('nginx', () => {
     it('should return 401 without credentials', async () => {
-        const res = await axios.get(`${BASE_URL}`);
-        expect(res).to.have.status(401);
+        let response;
+        try {
+            const res = await axios.get(`${BASE_URL}`);
+            res = res;
+        } catch (e) {
+            expect(e).to.have.property('response');
+            response = e.response;
+        }
+        expect(response).to.have.property('status');
+        expect(response.status).to.eql(401);
     });
 });
